ac8d97838fcf46dd0f493dfe8ce6dc1cb2b93d4d
Andrew Garcia <grcwebdev@gmail.com>
2019-11-12 19:11:39 -0500

    mockup responsive

--- src/app.js
+++ src/app.js
@@ -1,1 +1,70 @@
 import './scss/style.scss';
+
+// Make reload page go to top
+// window.onbeforeunload = function () {
+//     window.scrollTo(0);
+// }
+
+// Page Load Animation
+window.onload = function() {
+    document.getElementById('about-me-h3').classList.add('load');
+}
+
+// Mobile Nav Sticky
+window.onscroll = () => {
+    stickyFunction();
+}
+
+let navbar = document.getElementById('navbar');
+let sticky = navbar.offsetTop;
+
+function stickyFunction() {
+    if(window.pageYOffset >= sticky) {
+        if (window.innerWidth < 1024) {
+            navbar.classList.add('sticky');
+        }
+    } else {
+        navbar.classList.remove('sticky');
+    }
+}
+
+// Portfolio Filter
+let emailFilter = document.getElementById('email-filter');
+let landingFilter = document.getElementById('landing-filter');
+let javascriptFilter = document.getElementById('javascript-filter');
+
+emailFilter.addEventListener('click', function() {
+    clearFilter();
+    emailFilter.classList.add('active');
+    filterProjects('email');
+});
+
+landingFilter.addEventListener('click', function() {
+    clearFilter();
+    landingFilter.classList.add('active');
+    filterProjects('landing');
+});
+
+javascriptFilter.addEventListener('click', function() {
+    clearFilter();
+    javascriptFilter.classList.add('active');
+    filterProjects('javascript');
+});
+
+function clearFilter() {
+    emailFilter.classList.remove('active');
+    landingFilter.classList.remove('active');
+    javascriptFilter.classList.remove('active');
+}
+
+function filterProjects(filter) {
+    let grid = document.getElementById('portfolio-grid');
+    let childOfGrid = grid.children;
+    for(var i = 0; i < childOfGrid.length; i++) {
+        if(childOfGrid[i].classList[1] == filter) {
+            childOfGrid[i].style.display = 'block'
+        } else {
+            childOfGrid[i].style.display = 'none';
+        }
+    }
+}
